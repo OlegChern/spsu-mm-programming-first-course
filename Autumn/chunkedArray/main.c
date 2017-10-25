0400f23ce32dbc750f9ce314fbe9b110beff6323
Kirill <31822295+kirillgla@users.noreply.github.com>
2017-10-25 12:40:58 +0300

    - finished chunked array

---
+++ Autumn/chunkedArray/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#include "chunkedArray.h"
+
+int main()
+{
+    printf("Testing chunked array.\n");
+    ChunkedArray *array = buildChunkedArray();
+    printf("0th element before filling the array: %08X\n", at(array, 0));
+    for (int i = 0; i < 10; i++)
+    {
+        addToEnd(array, i);
+    }
+    addToEnd(array, VALUE_ERROR);
+    // 10 is intentionally skipped
+    for (int i = 11; i < 18; i++)
+    {
+        addToEnd(array, i);
+    }
+
+    printf("Array after filling:\n");
+    printArray(array);
+
+    printf("Getting all elements:\n");
+    int count = BASE_LENGTH * (array->linesNumber - 1) + array->lastLineLength;
+    printf("[");
+    for (int i = 0; i < count; i++)
+    {
+        printf(i == count - 1? "%d]\n" : "%d, ", at(array, i));
+    }
+    printf("Index out of range: %08X\n", at(array, count));
+
+    freeChunkedArray(array);
+    printf("Done.\n");
+    return 0;
+}
