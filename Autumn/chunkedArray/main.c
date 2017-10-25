fbd687696f7cb6d1a12a84347e2e48f4194808aa
Kirill <31822295+kirillgla@users.noreply.github.com>
2017-10-25 12:49:29 +0300

    - slightly improved output format

--- Autumn/chunkedArray/main.c
+++ Autumn/chunkedArray/main.c
@@ -7,7 +7,7 @@ int main()
     printf("\nTesting chunked array.\n");
     printf("Base length: %d\n", BASE_LENGTH);
     ChunkedArray *array = buildChunkedArray();
-    printf("0th element before filling the array: %08X\n", at(array, 0));
+    printf("0th element before filling the array: 0x%08X\n", at(array, 0));
     for (int i = 0; i < 10; i++)
     {
         addToEnd(array, i);
@@ -30,7 +30,7 @@ int main()
     {
         printf(i == count - 1? "%d]\n" : "%d, ", at(array, i));
     }
-    printf("Index out of range: %08X\n", at(array, count));
+    printf("Index out of range: 0x%08X\n", at(array, count));
 
     freeChunkedArray(array);
     printf("Done.\n");
