7720dd06abc76f2c9745ae3ba46f360c0e153899
Oleg <OLChernyavsky@mail.ru>
2017-11-13 21:33:12 +0300

    Minor changes

--- ex7/main.c
+++ ex7/main.c
@@ -7,16 +7,87 @@ struct list
     struct list* next;
 };
 
+
+struct hash_t
+{
+    struct list* arr_lst;
+    int size;
+    int counter;
+};
+
+
+int main() {
+
+    int sz;
+
+    printf("%s", "Please, enter the size of the hash-table you want to create: ");
+    scanf("%d", &sz);
+
+    struct hash_t* table;
+
+    table->size = sz;
+    table->counter = 0;
+    table->arr_lst = malloc(sizeof(struct list)*sz);
+
+    int input = -1;
+
+    while (input != 0)
+    {
+        printf("%s", "Please, choose an option: 1 - add an element, 2 - remove an element, 3 - search for an element, 4 - print the table, 0 - delete the table\n");
+        scanf("%d", &input);
+
+        switch(input) {
+            case 0:
+                hash_Delete(table);
+                break;
+            case 1: {
+                int key, value;
+                printf("%s\n", "Enter the key and the value: ");
+                scanf("%d %d", &key, &value);
+                hash_Add(table, key, value);
+                break;
+            }
+            case 4: {
+                hash_Print(table);
+                break;
+            }
+            case 2: {
+                int key;
+                scanf("%d", &key);
+                hash_Remove(table, key);
+                break;
+            }
+            case 3:
+            {
+                int key, value;
+                printf("%s\n", "Enter the key: ");
+                scanf("%d", &key);
+                if(hash_Search(table,key,&value))
+                {
+                    printf("%s%d\n","The value is ", value);
+                }
+                else
+                {
+                    printf("%s", "There is no match for this key\n");
+                }
+            }
+
+        }
+
+    }
+
+    return 0;
+}
+
 void list_AddToEnd (int add_value, int add_key, struct list* list)
 {
     struct list* l = list;
 
     while (l->next != NULL)
     {
-        if (l->value == add_value)
+        if (l->key == add_key)
         {
             l->value = add_value;
-            l->key = add_key;
             return;
         }
         l = l->next;
@@ -88,13 +159,6 @@ void list_Delete(struct list* list)
     free(list);
 
 }
-
-struct hash_t
-{
-    struct list* arr_lst;
-    int size;
-    int counter;
-};
 
 
 int hash_func (int key, struct hash_t* table)
@@ -196,64 +260,3 @@ void hash_Delete(struct hash_t* table)
 }
 
 
-
-int main() {
-
-    int sz;
-
-    printf("%s", "Please, enter the size of the hash-table you want to create: ");
-    scanf("%d", &sz);
-
-    struct hash_t* table;
-
-    table->size = sz;
-    table->counter = 0;
-    table->arr_lst = malloc(sizeof(struct list)*sz);
-
-    int input = -1;
-
-    while (input != 0)
-    {
-        printf("%s", "Please, choose an option: 1 - add an element, 2 - remove an element, 3 - search for an element, 4 - print the table, 0 - delete the table\n");
-        scanf("%d", &input);
-
-        switch(input) {
-            case 0:
-                hash_Delete(table);
-                break;
-            case 1: {
-                int key, value;
-                scanf("%d %d", &key, &value);
-                hash_Add(table, key, value);
-                break;
-            }
-            case 4: {
-                hash_Print(table);
-                break;
-            }
-            case 2: {
-                int key;
-                scanf("%d", &key);
-                hash_Remove(table, key);
-                break;
-            }
-            case 3:
-            {
-                int key, value;
-                scanf("%d", &key);
-                if(hash_Search(table,key,&value))
-                {
-                    printf("%d\n", value);
-                }
-                else
-                {
-                    printf("%s", "There is no match for this key\n");
-                }
-            }
-
-        }
-
-    }
-
-    return 0;
-}
