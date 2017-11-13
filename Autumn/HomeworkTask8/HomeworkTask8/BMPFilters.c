b00fc75d7278865645e158e48a4506eabb75eb20
sultim222 <sultim.tsyrendashiev@gmail.com>
2017-11-13 20:32:56 +0300

    - bug fixes - improved code readability

--- Autumn/HomeworkTask8/HomeworkTask8/BMPFilters.c
+++ Autumn/HomeworkTask8/HomeworkTask8/BMPFilters.c
@@ -1,384 +1,196 @@
+#pragma warning(disable:4996) // _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
 #include "BMPFilters.h"
 
-int main()
+int main(int argc, char **argv)
 {
-	printf("This program applies filter to image.\n");
-	printf("Source and target file must .BMP.\n\n");
-	printf("Available filters:\n0 = Average3x3 (default)\n");
-	printf("1 = Gaussian3x3\n2 = Sobel X\n3 = Sobel Y\n4 = GrayScale\n\n");
+	//Gauss
+	//SobelX
+	//SobelY 
+	//GrayScale
+	//Average
 
-	do {
+	if (argc < 4)
+	{
+		printf("Not enough arguments!");
+		return 0;
+	}
 
-		FILE *sourceFile;
-		BITMAPFILEHEADER bmFileHeader;
-		BITMAPINFOHEADER bmInfoHeader;
-		UBYTE *image;
+	FILE *sourceFile;
+	FILE *targetFile;
 
-		{
-			char* pathsource = malloc(sizeof(char) * 64);
-			printf("Enter path to source file: \n");
-			scanf("%s", pathsource);
-			sourceFile = fopen(pathsource, "rb");
-			free(pathsource);
-		}
+	BITMAPFILEHEADER bmFileHeader;
+	BITMAPINFOHEADER bmInfoHeader;
 
-		if (sourceFile == NULL)
-		{
-			printf("Wrong path!\n");
-			continue;
-		}
+	int bit32 = 0;
 
-		fread(&bmFileHeader, sizeof(BITMAPFILEHEADER), 1, sourceFile);
-		if (bmFileHeader.bfType != 0x4D42)
-		{
-			printf("Wrong file header type!\n");
-			continue;
-		}
+	sourceFile = fopen(argv[1], "rb");
+	if (sourceFile == NULL)
+	{
+		printf("Wrong path!\n");
+		return 0;
+	}
 
-		fread(&bmInfoHeader, sizeof(BITMAPINFOHEADER), 1, sourceFile);
-		if (bmInfoHeader.biSize != 0x28)
-		{
-			printf("Wrong info header size!\n");
-			continue;
-		}
-		fseek(sourceFile, bmFileHeader.bfOffBits, SEEK_SET);
+	targetFile = fopen(argv[2], "wb");
+	if (targetFile == NULL)
+	{
+		printf("Wrong path!\n");
+		return 0;
+	}
 
-		image = (UBYTE*)malloc(bmInfoHeader.biSizeImage);
-		if (!image)
+	fread(&bmFileHeader, sizeof(BITMAPFILEHEADER), 1, sourceFile);
+	if (bmFileHeader.bfType != 0x4D42)
+	{
+		printf("Wrong file header type!\n");
+		return 0;
+	}
+
+	fread(&bmInfoHeader, sizeof(BITMAPINFOHEADER), 1, sourceFile);
+	if (bmInfoHeader.biSize != 0x28)
+	{
+		printf("Wrong info header size!\n");
+		return 0;
+	}
+
+	fseek(sourceFile, bmFileHeader.bfOffBits, SEEK_SET);
+
+	// array from source file
+	UBYTE *image = (UBYTE*)malloc(bmInfoHeader.biSizeImage);
+	if (!image)
+	{
+		printf("Not enough memory!\n");
+		free(image);
+		return 0;
+	}
+
+	fread(image, bmInfoHeader.biSizeImage, 1, sourceFile);
+
+	int width = bmInfoHeader.biWidth;
+	int height = bmInfoHeader.biHeight;
+
+	// new image with RGB
+	PIXEL **newImage = (PIXEL**)malloc(sizeof(PIXEL*) * width);
+	if (!newImage)
+	{
+		printf("Not enough memory!\n");
+		return 0;
+	}
+
+	for (int i = 0; i < width; i++)
+	{
+		newImage[i] = (PIXEL*)malloc(sizeof(PIXEL) * height);
+		if (!newImage[i])
 		{
 			printf("Not enough memory!\n");
-			free(image);
-			continue;
+			free(newImage);
+			return 0;
 		}
-		
-		fread(image, bmInfoHeader.biSizeImage, 1, sourceFile);
-		if (image == NULL)
+	}
+
+	// padding
+	UBYTE pad[3] = { 0, 0, 0 };
+	int padSize = (4 - (width * 3) % 4) % 4;
+
+	fwrite(&bmFileHeader, sizeof(BITMAPFILEHEADER), 1, targetFile);
+	fwrite(&bmInfoHeader, sizeof(BITMAPINFOHEADER), 1, targetFile);
+
+	// pixel array
+	for (int x = 0; x < width; x++)
+	{
+		for (int y = 0; y < height; y++)
 		{
-			printf("Image is NULL!\n");
-			continue;
+			int offset = (x + y * width) * 3 + y * padSize;
+
+			newImage[x][y].red = image[offset + 2];
+			newImage[x][y].green = image[offset + 1];
+			newImage[x][y].blue = image[offset];
+		}
+	}
+
+	// applying filter
+	if (strcmp(argv[2], "Gauss") == 0) // argv[2] == "Gauss")
+	{
+		float sigma = 1;
+		sigma = 2 * sigma * sigma;
+
+		float **kernel = (float**)malloc(sizeof(float*) * 3);
+		for (char i = -1; i <= 1; i++)
+		{
+			kernel[i + 1] = (float*)malloc(sizeof(float) * 3);
+			for (char j = -1; j <= 1; j++)
+			{
+				kernel[i + 1][j + 1] = (float)((exp(-(i*i + j*j) / sigma)) / (PI * sigma));
+			}
 		}
 
-		// choosing filter
-		do {
+		gauss3x3(width, height, kernel, newImage);
+	}
+	else if (strcmp(argv[2], "SobelX") == 0) // argv[2] == "SobelX")
+	{
+		char maskx[3][3] =
+		{
+			{ -1, 0, 1 },
+			{ -2, 0, 2 },
+			{ -1, 0, 1 }
+		};
 
-			int filter = 0;
-			FILE *targetFile;
-			int width = bmInfoHeader.biWidth;
-			int height = bmInfoHeader.biHeight;
+		sobel(width, height, maskx, newImage);
+	}
+	else if (strcmp(argv[2], "SobelY") == 0) // argv[2] == "SobelY")
+	{
+		char masky[3][3] =
+		{
+			{ -1, -2, -1 },
+			{ 0, 0, 0 },
+			{ 1, 2, 1 }
+		};
 
-			{
-				char* pathtarget = malloc(sizeof(char) * 64);
-				printf("Enter path to target file : \n");
-				scanf("%s", pathtarget);
+		sobel(width, height, masky, newImage);
+	}
+	else if (strcmp(argv[2], "GrayScale") == 0) // argv[2] == "GrayScale")
+	{
+		grayscale(width, height, newImage);
+	}
+	else if (strcmp(argv[2], "Average") == 0) // argv[2] == "Average")
+	{
+		average3x3(width, height, newImage);
+	}
+	else
+	{
+		printf("Available filters:\n");
+		printf("- Gauss\n");
+		printf("- SobelX\n");
+		printf("- SobelY\n");
+		printf("- GrayScale\n");
+		printf("- Average\n");
+	}
 
-				targetFile = fopen(pathtarget, "wb");
-				free(pathtarget);
-			}
+	// write new image to file
+	fseek(targetFile, bmFileHeader.bfOffBits, SEEK_SET);
+	for (int y = 0; y < height; y++)
+	{
+		for (int x = 0; x < width; x++)
+		{
+			UBYTE b = newImage[x][y].blue;
+			UBYTE g = newImage[x][y].green;
+			UBYTE r = newImage[x][y].red;
 
-			if (targetFile == NULL)
-			{
-				printf("Wrong path!\n");
-				continue;
-			}
+			fwrite((UBYTE*)&b, 1, 1, targetFile);
+			fwrite((UBYTE*)&g, 1, 1, targetFile);
+			fwrite((UBYTE*)&r, 1, 1, targetFile);
+		}
+		fwrite((char*)pad, padSize, 1, targetFile);
+	}
 
-			// new image with RGB
-			PIXEL **newImage = (PIXEL**)malloc(sizeof(PIXEL*) * width);
-			if (!newImage)
-			{
-				printf("Not enough memory!\n");
-				continue;
-			}
+	free(image);
 
-			for (int i = 0; i < width; i++)
-			{
-				newImage[i] = (PIXEL*)malloc(sizeof(PIXEL) * height);
-				if (!newImage[i])
-				{
-					printf("Not enough memory!\n");
-					free(newImage);
-					continue;
-				}
-			}
+	fclose(sourceFile);
+	fclose(targetFile);
 
-			// for writing into file
-			UBYTE pad[3] = { 0, 0, 0 };
-			int padSize = (4 - (width * 3) % 4) % 4;
-			fwrite(&bmFileHeader, sizeof(BITMAPFILEHEADER), 1, targetFile);
-			fwrite(&bmInfoHeader, sizeof(BITMAPINFOHEADER), 1, targetFile);
-			// fseek(targetFile, bmFileHeader.bfOffBits, SEEK_SET);
-
-			// pixel array
-			for (int x = 0; x < width; x++)
-			{
-				for (int y = 0; y < height; y++)
-				{
-					int offset = (x + y*width) * 3 + y * padSize;
-
-					newImage[x][y].red = image[offset + 2];
-					newImage[x][y].green = image[offset + 1];
-					newImage[x][y].blue = image[offset];
-				}
-			}
-
-			{
-				do {
-
-					printf("Choose filter: ");
-					scanf("%d", &filter);
-					if (filter < 0 || filter > 4)
-					{
-						printf("Wrong filter index!\n");
-						continue;
-					}
-					break;
-
-				} while (1);
-			}
-
-			switch (filter)
-			{
-			case 1: // gaussian3x3
-			{
-				float sigma = 1;
-				sigma = 2 * sigma * sigma;
-				float **kernel = (float**)malloc(sizeof(float*) * 3);
-
-				for (char i = -1; i <= 1; i++)
-				{
-					kernel[i + 1] = (float*)malloc(sizeof(float) * 3);
-					for (char j = -1; j <= 1; j++)
-					{
-						{
-							kernel[i + 1][j + 1] = (exp(-(i*i + j*j) / sigma)) / (PI * sigma);
-						}
-					}
-				}
-
-				for (int x = 0; x < width; x++)
-				{
-					for (int y = 0; y < height; y++)
-					{
-						float sum = 0;
-						float r = 0, g = 0, b = 0;
-
-						for (int i = -1; i <= 1; i++)
-						{
-							for (int j = -1; j <= 1; j++)
-							{
-								if (x + i >= 0 && x + i < width && y + j >= 0 && y + j < height)
-								{
-									PIXEL pix = newImage[x + i][y + j];
-
-									r += pix.red * kernel[i + 1][j + 1];
-									g += pix.green * kernel[i + 1][j + 1];
-									b += pix.blue * kernel[i + 1][j + 1];
-
-									sum += kernel[i + 1][j + 1];
-								}
-							}
-						}
-
-						if (sum != 0)
-						{
-							newImage[x][y].red = round(r / sum);
-							newImage[x][y].green = round(g / sum);
-							newImage[x][y].blue = round(b / sum);
-						}
-					}
-				}
-
-				break;
-			}
-			case 2: // sobelx
-			{
-				char mask[3][3] =
-				{
-					{ -1, 0, 1 },
-					{ -2, 0, 2 },
-					{ -1, 0, 1 }
-				};
-				//sobel(width, height, newImage, masky);
-
-				for (int x = 0; x < width; x++)
-				{
-					for (int y = 0; y < height; y++)
-					{
-						short g = 0;
-
-						for (int i = -1; i <= 1; i++)
-						{
-							for (int j = -1; j <= 1; j++)
-							{
-								if (x + i >= 0 && x + i < width && y + j >= 0 && y + j < height)
-								{
-									UBYTE cur = pixelGrayscaled(newImage[x + i][y + j]);
-									g += cur * mask[i + 1][j + 1];
-								}
-							}
-						}
-
-						clampToByte(&g);
-						newImage[x][y] = *(pixelNewSame(g));
-					}
-				}
-
-				break;
-			}
-			case 3: // sobely
-			{
-				char mask[3][3] =
-				{
-					{ -1, -2, -1 },
-					{ 0, 0, 0 },
-					{ 1, 2, 1 }
-				};
-
-				for (int x = 0; x < width; x++)
-				{
-					for (int y = 0; y < height; y++)
-					{
-						short g = 0;
-
-						for (int i = -1; i <= 1; i++)
-						{
-							for (int j = -1; j <= 1; j++)
-							{
-								if (x + i >= 0 && x + i < width && y + j >= 0 && y + j < height)
-								{
-									UBYTE cur = pixelGrayscaled(newImage[x + i][y + j]);
-									g += cur * mask[i + 1][j + 1];
-								}
-							}
-						}
-
-						clampToByte(&g);
-						newImage[x][y] = *(pixelNewSame(g));
-					}
-				}
-
-				break;
-			}
-			case 4: // grayscale
-			{
-				for (int x = 0; x < width; x++)
-				{
-					for (int y = 0; y < height; y++)
-					{
-						/*PIXEL pix = newImage[x][y];
-						WORD grayscale = 0; // WORD used, because 255*9 > 255
-						grayscale = pix.blue + pix.green + pix.red;
-						grayscale /= 3;*/
-
-						UBYTE grayscale = pixelGrayscaled(newImage[x][y]);
-						newImage[x][y] = *(pixelNewSame(grayscale));
-					}
-				}
-				break;
-			}
-			default: // avg3x3
-			{
-				for (int x = 0; x < width; x++)
-				{
-					for (int y = 0; y < height; y++)
-					{
-						WORD r = 0, g = 0, b = 0; // WORD used, because 255*9 > 255
-						for (int i = -1; i <= 1; i++)
-						{
-							for (int j = -1; j <= 1; j++)
-							{
-								if (x + i >= 0 && x + i < width && y + j >= 0 && y + j < height)
-								{
-									r += newImage[x + i][y + j].red;
-									g += newImage[x + i][y + j].green;
-									b += newImage[x + i][y + j].blue;
-								}
-							}
-						}
-						newImage[x][y] = *(pixelNew(r / 9, g / 9, b / 9));
-					}
-				}
-				break;
-			}
-			}
-
-			// write new image to file
-			// fseek(targetFile, bmFileHeader.bfOffBits, SEEK_SET);
-			for (int y = 0; y < height; y++)
-			{
-				for (int x = 0; x < width; x++)
-				{
-					UBYTE b = newImage[x][y].blue;
-					UBYTE g = newImage[x][y].green;
-					UBYTE r = newImage[x][y].red;
-
-					fwrite((UBYTE*)&b, 1, 1, targetFile);
-					fwrite((UBYTE*)&g, 1, 1, targetFile);
-					fwrite((UBYTE*)&r, 1, 1, targetFile);
-				}
-				fwrite((char*)pad, padSize, 1, targetFile);
-			}
-
-			free(image);
-			fclose(targetFile);
-
-			printf("Done!\n");
-
-			break;
-
-		} while (1);
-
-		fclose(sourceFile);
-		break;
-
-	} while (1);
+	printf("Done!\n");
 
 	return 0;
-}
-
-PIXEL* pixelNew(UBYTE r, UBYTE g, UBYTE b)
-{
-	PIXEL* p = malloc(sizeof(PIXEL));
-	p->red = r;
-	p->green = g;
-	p->blue = b;
-	return p;
-}
-
-PIXEL* pixelNewSame(UBYTE v)
-{
-	PIXEL* p = malloc(sizeof(PIXEL));
-	p->red = v;
-	p->green = v;
-	p->blue = v;
-	return p;
-}
-
-UBYTE* pixelGrayscaled(PIXEL p)
-{
-	return (p.blue + p.green + p.red) / 3;
-}
-
-/*void pixelMultiply(PIXEL *p, float v)
-{
-	short value = p->red * v;
-	clampToByte(value);
-	p->red = value;
-
-	value = p->green * v;
-	clampToByte(value);
-	p->green = value;
-
-	value = p->blue * v;
-	clampToByte(value);
-	p->blue = value;
-}*/
-
-void clampToByte(short *value)
-{
-	*value = 
-		*value < 0 ? 0 : 
-		*value > 255 ? 255 : 
-		*value;
 }
--- Autumn/HomeworkTask8/HomeworkTask8/BMPFilters.h
+++ Autumn/HomeworkTask8/HomeworkTask8/BMPFilters.h
@@ -1,37 +1,42 @@
 #pragma once
 
+#include <stdlib.h>
+
 #define PI 3.14159
+#define FALSE 1
+#define TRUE 1
+#define CHUNK 8
 
 typedef unsigned char UBYTE;
-typedef unsigned short WORD;
-typedef unsigned long DWORD;
+typedef unsigned short USHORT;
+typedef unsigned long ULONG;
 typedef long LONG;
 
 #pragma pack(1)
 typedef struct
 {
-	WORD bfType;
-	DWORD bfSize;
-	WORD bfReserved1;
-	WORD bfReserved2;
-	DWORD bfOffBits;
+	USHORT bfType;
+	ULONG bfSize;
+	USHORT bfReserved1;
+	USHORT bfReserved2;
+	ULONG bfOffBits;
 } BITMAPFILEHEADER;
 #pragma pack()
 
 #pragma pack(1)
 typedef struct
 {
-	DWORD biSize;
+	ULONG biSize;
 	LONG biWidth;
 	LONG biHeight;
-	WORD biPlanes;
-	WORD biBitCount;
-	DWORD biCompression;
-	DWORD biSizeImage;
+	USHORT biPlanes;
+	USHORT biBitCount;
+	ULONG biCompression;
+	ULONG biSizeImage;
 	LONG biXPelsPerMeter;
 	LONG biYPelsPerMeter;
-	DWORD biClrUsed;
-	DWORD biClrImportant;
+	ULONG biClrUsed;
+	ULONG biClrImportant;
 } BITMAPINFOHEADER;
 #pragma pack()
 
@@ -42,8 +47,152 @@ typedef struct
 	UBYTE blue;
 } PIXEL;
 
-PIXEL* pixelNew(UBYTE, UBYTE, UBYTE);
-PIXEL* pixelNewSame(UBYTE);
-UBYTE* pixelGrayscaled(PIXEL);
-//void pixelMultiply(PIXEL*, float);
-void clampToByte(short*);
+PIXEL  *pixelNew(UBYTE, UBYTE, UBYTE);
+PIXEL *pixelNewSame(UBYTE);
+UBYTE pixelGrayscaled(PIXEL);
+UBYTE clampToUByte(short);
+
+void gauss3x3(int, int, float**, PIXEL**);
+void sobel(int, int, char[3][3], PIXEL**);
+void grayscale(int, int, PIXEL**);
+void average3x3(int, int, PIXEL**);
+
+PIXEL *pixelNew(UBYTE r, UBYTE g, UBYTE b)
+{
+	PIXEL* p = (PIXEL*)malloc(sizeof(PIXEL));
+	p->red = r;
+	p->green = g;
+	p->blue = b;
+	return p;
+}
+
+PIXEL *pixelNewSame(UBYTE v)
+{
+	PIXEL* p = (PIXEL*)malloc(sizeof(PIXEL));
+	p->red = v;
+	p->green = v;
+	p->blue = v;
+	return p;
+}
+
+UBYTE pixelGrayscaled(PIXEL p)
+{
+	return (UBYTE)((p.blue + p.green + p.red) / 3);
+}
+
+UBYTE clampToUByte(short value)
+{
+	UBYTE clamped =
+		value < 0 ? 0 : 
+		value > 255 ? 255 : 
+		(UBYTE)value;
+
+	return clamped;
+}
+
+UBYTE roundUBYTE(float f)
+{
+	UBYTE rounded = 
+		f - (float)((int)f) >= 0.5f ? (int)f + 1 : 
+		(int)f;
+
+	return rounded;
+}
+
+void gauss3x3(int width, int height, float **kernel, PIXEL **newImage)
+{
+	for (int x = 0; x < width; x++)
+	{
+		for (int y = 0; y < height; y++)
+		{
+			float sum = 0;
+			float r = 0, g = 0, b = 0;
+
+			for (int i = -1; i <= 1; i++)
+			{
+				for (int j = -1; j <= 1; j++)
+				{
+					if (x + i >= 0 && x + i < width && y + j >= 0 && y + j < height)
+					{
+						PIXEL pix = newImage[x + i][y + j];
+
+						r += pix.red * kernel[i + 1][j + 1];
+						g += pix.green * kernel[i + 1][j + 1];
+						b += pix.blue * kernel[i + 1][j + 1];
+
+						sum += kernel[i + 1][j + 1];
+					}
+				}
+			}
+
+			if (sum != 0)
+			{
+				newImage[x][y].red = roundUBYTE(r / sum);
+				newImage[x][y].green = roundUBYTE(g / sum);
+				newImage[x][y].blue = roundUBYTE(b / sum);
+			}
+		}
+	}
+}
+
+void sobel(int width, int height, char mask[3][3], PIXEL **newImage)
+{
+	for (int x = 0; x < width; x++)
+	{
+		for (int y = 0; y < height; y++)
+		{
+			short sum = 0;
+
+			for (int i = -1; i <= 1; i++)
+			{
+				for (int j = -1; j <= 1; j++)
+				{
+					if (x + i >= 0 && x + i < width && y + j >= 0 && y + j < height)
+					{
+						UBYTE cur = pixelGrayscaled(newImage[x + i][y + j]);
+						sum += cur * mask[i + 1][j + 1];
+					}
+				}
+			}
+
+			UBYTE clamped = clampToUByte(sum);
+			newImage[x][y] = *(pixelNewSame(clamped));
+		}
+	}
+}
+
+void grayscale(int width, int height, PIXEL **newImage)
+{
+	for (int x = 0; x < width; x++)
+	{
+		for (int y = 0; y < height; y++)
+		{
+			UBYTE grayscale = pixelGrayscaled(newImage[x][y]);
+			newImage[x][y] = *(pixelNewSame(grayscale));
+		}
+	}
+}
+
+void average3x3(int width, int height, PIXEL **newImage)
+{
+	for (int x = 0; x < width; x++)
+	{
+		for (int y = 0; y < height; y++)
+		{
+			USHORT r = 0, g = 0, b = 0; // USHORT used, because 255*9 > 255
+			for (int i = -1; i <= 1; i++)
+			{
+				for (int j = -1; j <= 1; j++)
+				{
+					if (x + i >= 0 && x + i < width && y + j >= 0 && y + j < height)
+					{
+						r += newImage[x + i][y + j].red;
+						g += newImage[x + i][y + j].green;
+						b += newImage[x + i][y + j].blue;
+					}
+				}
+			}
+			newImage[x][y] = *(pixelNew(r / 9, g / 9, b / 9));
+		}
+	}
+}
