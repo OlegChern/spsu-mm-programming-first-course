13e7be35ea936c7b607843a4d0163045fc07d09c
sultim222 <sultim.tsyrendashiev@gmail.com>
2017-12-02 21:11:58 +0300

    - fixed header file - bug fixes

--- Autumn/HomeworkTask10/HomeworkTask10/Coins.c
+++ Autumn/HomeworkTask10/HomeworkTask10/Coins.c
@@ -42,3 +42,66 @@ int count(int n, int arr[])
 
 	return sum;
 }
+
+int getInt(int *target)
+{
+	while (TRUE)
+	{
+		char current;
+		size_t length = 0, size = CHUNK;
+
+		char *source = (char*)malloc(sizeof(char) * size);
+		if (!*source)
+		{
+			printf("Can't allocate!\n");
+			return FALSE;
+		}
+
+		while (TRUE)
+		{
+			current = getchar();
+
+			if (current == '\n') // if end of the line
+			{
+				if (length > 0)
+				{
+					source[length] = '\0';
+					(*target) = atoi(source);
+
+					if (*target != 0)
+					{
+						return TRUE;
+					}
+					else
+					{
+						return FALSE;
+					}
+				}
+
+				return FALSE;
+			}
+
+			if (current < '0' || current > '9') // just ignore
+			{
+				continue;
+			}
+
+			source[length++] = current;
+
+			if (size <= length) // if allocated size <= length of char array
+			{
+				size += CHUNK;
+				source = realloc(source, sizeof(char) * size); // new size of char array
+
+				if (!*source)
+				{
+					printf("Can't reallocate!\n");
+					free(source);
+					return FALSE;
+				}
+			}
+		}
+	}
+
+	return FALSE;
+}
--- Autumn/HomeworkTask8/HomeworkTask8/BMPFilters.c
+++ Autumn/HomeworkTask8/HomeworkTask8/BMPFilters.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <stdarg.h>
 #include "BMPFilters.h"
 
 int main(int argc, char **argv)
@@ -49,7 +50,7 @@ int main(int argc, char **argv)
 	{
 		printf("Wrong target path!\n");
 
-		fclose(sourceFile);
+		closeFiles(1, sourceFile);
 
 		return 0;
 	}
@@ -59,7 +60,7 @@ int main(int argc, char **argv)
 	{
 		printf("Wrong file header type!\n");
 
-		closeFiles(sourceFile, targetFile);
+		closeFiles(2, sourceFile, targetFile);
 
 		return 0;
 	}
@@ -69,7 +70,7 @@ int main(int argc, char **argv)
 	{
 		printf("Wrong info header size!\n");
 
-		closeFiles(sourceFile, targetFile);
+		closeFiles(2, sourceFile, targetFile);
 
 		return 0;
 	}
@@ -82,7 +83,7 @@ int main(int argc, char **argv)
 	{
 		printf("Not enough memory!\n");
 
-		closeFiles(sourceFile, targetFile);
+		closeFiles(2, sourceFile, targetFile);
 
 		return 0;
 	}
@@ -98,7 +99,7 @@ int main(int argc, char **argv)
 	if (!createImage(width, height, newImage))
 	{
 		free(image);
-		closeFiles(sourceFile, targetFile);
+		closeFiles(2, sourceFile, targetFile);
 	}
 
 	// padding
@@ -142,8 +143,17 @@ int main(int argc, char **argv)
 
 		if (!gauss3x3(width, height, kernel, newImage))
 		{
-			closeAll(sourceFile, targetFile, image, newImage);
+			free(image);
+			closeImage(width, newImage);
+			closeFiles(2, sourceFile, targetFile);
 		}
+
+		for (char i = 0; i < 3; i++)
+		{
+			free(kernel[i]);
+		}
+
+		free(kernel);
 	}
 	else if (compare(argv[2], "SobelX"))
 	{
@@ -158,7 +168,9 @@ int main(int argc, char **argv)
 
 		if (!sobel(width, height, maskx, newImage))
 		{
-			closeAll(sourceFile, targetFile, image, newImage);
+			free(image);
+			closeImage(width, newImage);
+			closeFiles(2, sourceFile, targetFile);
 		}
 	}
 	else if (compare(argv[2], "SobelY"))
@@ -174,7 +186,9 @@ int main(int argc, char **argv)
 
 		if (!sobel(width, height, masky, newImage))
 		{
-			closeAll(sourceFile, targetFile, image, newImage);
+			free(image);
+			closeImage(width, newImage);
+			closeFiles(2, sourceFile, targetFile);
 		}
 	}
 	else if (compare(argv[2], "GrayScale"))
@@ -198,7 +212,9 @@ int main(int argc, char **argv)
 		printf("- GrayScale\n");
 		printf("- Average\n");
 
-		closeAll(sourceFile, targetFile, image, newImage);
+		free(image);
+		closeImage(width, newImage);
+		closeFiles(2, sourceFile, targetFile);
 
 		return 0;
 	}
@@ -226,9 +242,215 @@ int main(int argc, char **argv)
 		fwrite((char*)pad, padSize, 1, targetFile);
 	}
 
-	closeAll(sourceFile, targetFile, image, newImage);
+	free(image);
+	closeImage(width, newImage);
+	closeFiles(2, sourceFile, targetFile);
 
 	printf("Done!\n");
 
 	return 0;
+}
+
+int createImage(int width, int height, PIXEL **target)
+{
+	if (!target)
+	{
+		printf("Not enough memory!\n");
+		return 0;
+	}
+
+	for (int i = 0; i < width; i++)
+	{
+		target[i] = (PIXEL*)malloc(sizeof(PIXEL) * height);
+		if (!target[i])
+		{
+			printf("Not enough memory!\n");
+			return 0;
+		}
+	}
+
+	return 1;
+}
+
+void closeImage(int width, PIXEL **target)
+{
+	for (int i = 0; i < width; i++)
+	{
+		free(target[i]);
+	}
+
+	free(target);
+}
+
+PIXEL *pixelNew(UBYTE r, UBYTE g, UBYTE b)
+{
+	PIXEL* p = (PIXEL*)malloc(sizeof(PIXEL));
+	p->red = r;
+	p->green = g;
+	p->blue = b;
+	return p;
+}
+
+PIXEL *pixelNewSame(UBYTE v)
+{
+	PIXEL* p = (PIXEL*)malloc(sizeof(PIXEL));
+	p->red = v;
+	p->green = v;
+	p->blue = v;
+	return p;
+}
+
+UBYTE pixelGrayscaled(PIXEL p)
+{
+	return (UBYTE)((p.blue + p.green + p.red) / 3);
+}
+
+UBYTE clampToUByte(short value)
+{
+	UBYTE clamped =
+		value < 0 ? 0 :
+		value > 255 ? 255 :
+		(UBYTE)value;
+
+	return clamped;
+}
+
+UBYTE roundUBYTE(float f)
+{
+	UBYTE rounded =
+		f - (float)((int)f) >= 0.5f ? (int)f + 1 :
+		(int)f;
+
+	return rounded;
+}
+
+int gauss3x3(int width, int height, float **kernel, PIXEL **newImage)
+{
+	for (int x = 1; x < width - 1; x++)
+	{
+		for (int y = 1; y < height - 1; y++)
+		{
+			float sum = 0;
+			float r = 0, g = 0, b = 0;
+
+			for (int i = -1; i <= 1; i++)
+			{
+				for (int j = -1; j <= 1; j++)
+				{
+					PIXEL pix = newImage[x + i][y + j];
+
+					r += pix.red * kernel[i + 1][j + 1];
+					g += pix.green * kernel[i + 1][j + 1];
+					b += pix.blue * kernel[i + 1][j + 1];
+
+					sum += kernel[i + 1][j + 1];
+				}
+			}
+
+			if (sum != 0)
+			{
+				newImage[x][y].red = roundUBYTE(r / sum);
+				newImage[x][y].green = roundUBYTE(g / sum);
+				newImage[x][y].blue = roundUBYTE(b / sum);
+			}
+		}
+	}
+
+	return 1;
+}
+
+int sobel(int width, int height, char mask[3][3], PIXEL **targetImage)
+{
+	PIXEL **temp = (PIXEL**)malloc(sizeof(PIXEL*) * width);
+	if (!createImage(width, height, temp))
+	{
+		printf("Not enough memory!\n");
+		return 0;
+	}
+
+	for (int x = 1; x < width - 1; x++)
+	{
+		for (int y = 1; y < height - 1; y++)
+		{
+			short sum = 0;
+
+			for (int i = -1; i <= 1; i++)
+			{
+				for (int j = -1; j <= 1; j++)
+				{
+					UBYTE cur = pixelGrayscaled(targetImage[x + i][y + j]);
+					sum += cur * mask[i + 1][j + 1];
+				}
+			}
+
+			UBYTE clamped = clampToUByte(sum);
+			temp[x][y] = *(pixelNewSame(clamped));
+		}
+	}
+
+	for (int x = 0; x < width; x++)
+	{
+		for (int y = 0; y < height; y++)
+		{
+			targetImage[x][y] = temp[x][y];
+		}
+	}
+
+	return 1;
+}
+
+int grayscale(int width, int height, PIXEL **newImage)
+{
+	for (int x = 0; x < width; x++)
+	{
+		for (int y = 0; y < height; y++)
+		{
+			UBYTE grayscale = pixelGrayscaled(newImage[x][y]);
+			newImage[x][y] = *(pixelNewSame(grayscale));
+		}
+	}
+
+	return 1;
+}
+
+int average3x3(int width, int height, PIXEL **newImage)
+{
+	for (int x = 1; x < width - 1; x++)
+	{
+		for (int y = 1; y < height - 1; y++)
+		{
+			USHORT r = 0, g = 0, b = 0; // USHORT used, because 255*9 > 255
+			for (int i = -1; i <= 1; i++)
+			{
+				for (int j = -1; j <= 1; j++)
+				{
+					r += newImage[x + i][y + j].red;
+					g += newImage[x + i][y + j].green;
+					b += newImage[x + i][y + j].blue;
+				}
+			}
+			newImage[x][y] = *(pixelNew(r / 9, g / 9, b / 9));
+		}
+	}
+
+	return 1;
+}
+
+int compare(const char *c1, const char*c2)
+{
+	return strcmp(c1, c2) == 0;
+}
+
+void closeFiles(int count, ...)
+{
+	va_list files;
+
+	va_start(files, count);
+
+	for (int i = 0; i < count; i++)
+	{
+		fclose(va_arg(files, FILE*));
+	}
+
+	va_end(files);
 }
