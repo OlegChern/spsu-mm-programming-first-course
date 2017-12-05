fceab710cf61af93be69c619f37f2f2e58e040e6
Kirill <31822295+kirillgla@users.noreply.github.com>
2017-12-05 21:40:14 +0300

    - reshaped comments

--- Autumn/strcpy/smartstrcpy.c
+++ Autumn/strcpy/smartstrcpy.c
@@ -13,15 +13,29 @@ char *dumbstrcpy(char *dst, const char *src)
     return dst;
 }
 
+/**
+ * Following optimisations
+ * are currently present in the main loop:
+ * - manually unrolled loop
+ * - managing data as integer type
+ *   (since byte handling speed
+ *    is typically not faster than
+ *    the one of handling integers)
+ */
 char *smartstrcpy(char *dst, const char *src)
 {
+    // This value has to be saved
+    // so that to be returned later on
     char *initial_dst = dst;
-    int len = strlen(src);
+
+    // "+ 1" here ensures '\0' is copied as well.
+    int len = strlen(src) + 1;
     int len_32 = len / 32;
     int len_not_32 = len % 32;
 
-    // Ensures '\0' is copied.
-    *(dst++) = *(src++);
+    // Yet another loop unrolling
+    // could have been implemented here,
+    // but it is hardly worth it
 
     // Copy items that can't be handled
     // by the main loop due to data shape mismatch
@@ -33,12 +47,7 @@ char *smartstrcpy(char *dst, const char *src)
     int *src_current = (int*) src;
     int *dst_current = (int*) dst;
 
-    // Following optimisations are currently present:
-    // - manually unrolled loop
-    // - managing data as integer type
-    //   (since byte handling speed
-    //    is typically not faster than
-    //    the one of handling integers)
+    // The main loop:
     for (int i = 0; i < len_32; i++)
     {
         *(dst++) = *(src++);
