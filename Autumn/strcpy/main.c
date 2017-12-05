640a834a7f3871fcbbea474857f10b6d6e38ce0d
Kirill <31822295+kirillgla@users.noreply.github.com>
2017-12-05 21:22:51 +0300

    - performed a minor cleanup

--- Autumn/strcpy/main.c
+++ Autumn/strcpy/main.c
@@ -9,7 +9,7 @@
 #define STATS() printf("%.02f seconds.\n", (double) (end - start) / CLOCKS_PER_SEC)
 
 const char *src = "In the C programming language, Duff's device is a way of manually implementing loop unrolling by interleaving two syntactic constructs of C: the do-while loop and a switch statement. Its discovery is credited to Tom Duff in November 1983, when Duff was working for Lucasfilm and used it to speed up a real-time animation program. Loop unrolling attempts to reduce the overhead of conditional branching needed to check whether a loop is done, by executing a batch of loop bodies per iteration. To handle cases where the number of iterations is not divisible by the unrolled-loop increments, a common technique among assembly language programmers is to jump directly into the middle of the unrolled loop body to handle the remainder. Duff implemented this technique in C by using C's case label fall-through feature to jump into the unrolled body.";
-const int count = 2000000;
+const int count = 4000000;
 
 int main(void)
 {
@@ -43,7 +43,6 @@ int main(void)
         smartstrcpy(dst, src);
     end = clock();
     STATS();
-    printf(dst);
 
     return 0;
 }
