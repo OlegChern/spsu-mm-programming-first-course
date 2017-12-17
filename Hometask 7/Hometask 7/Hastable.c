72d254b98c50d56e572ebe4a7daeb35c5a93f2a0
bekzatalenov <bekzat.alenov@gmail.com>
2017-12-17 18:19:49 +0300

    -added changed my hometask 7

---
+++ Hometask 7/Hometask 7/Hastable.c
@@ -0,0 +1,110 @@
+#include <stdlib.h>
+#include <string.h>
+#include "Hashtable.h"
+
+NODE **getNode(HASHTABLE *, char*);
+int hash(char*);
+void recalc(HASHTABLE *);
+
+HASHTABLE *create()
+{
+	HASHTABLE *ht = malloc(sizeof(HASHTABLE));
+	if (ht == 0)
+	{
+		return 0;
+	}
+	ht->size = 0;
+	ht->capacity = INITIAL_CAPACITY;
+	ht->table = calloc(INITIAL_CAPACITY, sizeof(NODE*));
+	if (ht->table == 0)
+	{
+		return 0;
+	}
+	return ht;
+}
+
+void *get(HASHTABLE *ht, char *key)
+{
+	NODE **node = getNode(ht, key);
+	return node ? (*node)->value : 0;
+}
+
+void put(HASHTABLE *ht, char *key, void *value)
+{
+	NODE **node = getNode(ht, key);
+	if (node)
+	{
+		(*node)->value = value;
+	}
+	else
+	{
+		int h = hash(key) & (ht->capacity - 1);
+		NODE *newNode = malloc(sizeof(NODE));
+		newNode->key = key;
+		newNode->value = value;
+		newNode->next = ht->table[h];
+		ht->table[h] = newNode;
+		ht->size++;
+		if (ht->size / (double)(ht->capacity) > RECALC)
+		{
+			recalc(ht);
+		}
+	}
+}
+
+void delete(HASHTABLE *ht, char *key)
+{
+	NODE **node = getNode(ht, key);
+	if (node)
+	{
+		NODE *oldNode = *node;
+		*node = oldNode->next;
+		free(oldNode);
+	}
+}
+
+int hash(char *key)
+{
+	int s = 0;
+	while (*key)
+	{
+		s *= 19;
+		s += *key++;
+	}
+	return s;
+}
+
+NODE **getNode(HASHTABLE *ht, char *key)
+{
+	int h = hash(key) & (ht->capacity - 1);
+	NODE **node = ht->table + h;
+	while (*node && strcmp(key, (*node)->key) != 0)
+	{
+		node = &((*node)->next);
+	}
+	return *node ? node : 0;
+}
+
+void recalc(HASHTABLE *ht)
+{
+	int i;
+	if (ht->capacity == MAX_CAPACITY) return;
+	NODE **oldTable = ht->table;
+	ht->table = calloc(ht->capacity <<= 1, sizeof(NODE*));
+	ht->size = 0;
+	for (i = 0; i < (ht->capacity >> 1); i++)
+	{
+		NODE *node = oldTable[i];
+		while (node)
+		{
+			NODE *oldNode = node;
+			node = node->next;
+			put(ht, oldNode->key, oldNode->value);
+			free(oldNode);
+		}
+	}
+}
+
+
+
+
---
+++ Hometask 7/Hometask 7/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Hashtable.h"
+#define _CRT_NO_WARNINGS
+
+int main()
+{
+	int i;
+	HASHTABLE *ht = create();
+	put(ht, "key1", "value1");
+	put(ht, "key2", "value2");
+	put(ht, "key3", "value3");
+	put(ht, "key4", "value4");
+	put(ht, "key3", "value5");
+	printf("%s\n", (char*)get(ht, "key1"));
+	printf("%s\n", (char*)get(ht, "key2"));
+	printf("%s\n", (char*)get(ht, "key3"));
+	printf("%s\n", (char*)get(ht, "key4"));
+	void *f = get(ht, "kkk");
+	if (f)
+	{
+		printf("Error!\n");
+	}
+	else
+	{
+		printf("Not found!\n");
+	}
+	for (i = 0; i < 50; i++)
+	{
+		char *s = calloc(6, 1);
+		sprintf_s(s, 5, "%d", i);
+		put(ht, s, s);
+	}
+
+	return 0;
+}
