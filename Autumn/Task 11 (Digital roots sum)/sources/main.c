f87a3c171a7accec5ef38cfd4bc9a81e6f4e612e
Aleksandr <bash.spbu@gmail.com>
2017-12-22 18:51:17 +0300

    Fixed errors in Task 11 (Digitak roots sum)

--- Autumn/Task 11 (Digital roots sum)/sources/main.c
+++ Autumn/Task 11 (Digital roots sum)/sources/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <memory.h>
-#include <stdlib.h>
 
-#define MAX_BOUND 999999
+#define MAX_BOUND 1000000 // exclusive
+
+#define max(a, b) (((a) > (b)) ? (a) : (b))
 
 
 int digitSum(int n)
@@ -17,11 +17,6 @@ int digitSum(int n)
     return sum;
 }
 
-int max(int a, int b)
-{
-    return a >= b ? a : b;
-}
-
 
 int main()
 {
@@ -32,53 +27,29 @@ int main()
         "#---------------------------- END OF USAGE ------------------------------#\n\n"
     );
 
-    int digitalRoot[MAX_BOUND + 1];
-    int digitalRootLen = MAX_BOUND + 1;
+    int digitalRoot[MAX_BOUND];
+    int mdrs[MAX_BOUND];
 
+    // Bases of dynamic programming
     for (int i = 0; i < 10; ++i)
         digitalRoot[i] = i;
+    mdrs[0] = digitalRoot[0];
+    mdrs[1] = digitalRoot[1];
 
-    for (int i = 10; i < digitalRootLen; ++i)
-        digitalRoot[i] = digitalRoot[digitSum(i)];
-
-    int primes[MAX_BOUND + 1];
-    int primesLen = MAX_BOUND + 1;
-
-    memset(primes, 1, sizeof(primes[0]) * primesLen);
-    primes[0] = primes[1] = 0;
-    for (int i = 2; i * i <= primesLen; ++i)
-        if (primes[i])
-            for (int j = i * i; j < primesLen; j += i)
-                primes[j] = 0;
-
-    int mdrs[MAX_BOUND + 1];
-    int mdrsLen = MAX_BOUND + 1;
-
-    for (int i = 2; i < mdrsLen; ++i)
+    // Computing all values
+    for (int i = 2; i < MAX_BOUND; ++i)
     {
-        if (primes[i])
-            mdrs[i] = digitalRoot[i];
-        else
-        {
-            int t = i;
-            for (int j = 2; j * j <= i; ++j)
-                if (i % j == 0)
-                {
-                    mdrs[i] = max(mdrs[i], mdrs[i / j] + digitalRoot[j]);
-                    while (t % j == 0)
-                        t /= j;
-                }
-
-            if (t != 1)
-                mdrs[i] = max(mdrs[i], mdrs[i / t] + digitalRoot[t]);
-        }
+        mdrs[i] = digitalRoot[i] = digitalRoot[digitSum(i)];
+        for (int j = 2; j * j <= i; ++j)
+            if (i % j == 0)
+                mdrs[i] = max(mdrs[i], mdrs[j] + mdrs[i / j]);
     }
 
     int result = 0;
-    for (int i = 2; i < mdrsLen; ++i)
+    for (int i = 2; i < MAX_BOUND; ++i)
         result += mdrs[i];
 
     printf("The requested sum is: %d.\n", result);
-    
+
     return 0;
 }
