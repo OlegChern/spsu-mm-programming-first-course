5ed37b29f316b298875fc9de6e753a847b96d5db
Kirill <kirill_gla@mail.ru>
2018-02-25 00:09:49 +0300

    - created task2 project

---
+++ Spring/task2/task2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main()
+        {
+            new task2Lib.Tank();
+            Console.WriteLine("Hello, world!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Spring/task2/task2Lib/Tank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2Lib
+{
+    public class Tank
+    {
+        public Tank()
+        {
+            Console.WriteLine("Creating Tank!");
+        }
+    }
+}
