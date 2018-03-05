6239942e4516dfb400217853e88380f9a4f5f3dd
Kirill <kirill_gla@mail.ru>
2018-03-05 20:04:59 +0300

    - started task5 as Rider project

---
+++ Spring/task5/task5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace task5
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, world!");
+            Console.ReadKey();
+        }
+    }
+}
