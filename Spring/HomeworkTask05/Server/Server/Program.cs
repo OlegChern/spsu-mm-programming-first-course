471c00947603bc1ea0d1abc99d9fff1aea2fc8a9
sultim222 <sultim.tsyrendashiev@gmail.com>
2018-03-18 21:57:31 +0300

    - added task 5

---
+++ Spring/HomeworkTask05/Server/Server/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+	public class Program
+	{
+		static Server server;
+		static Thread thread;
+
+		public static void Main()
+		{
+			try
+			{
+				server = new Server();
+				thread = new Thread(new ThreadStart(server.StartReceiving));
+				thread.Start();
+			}
+			catch (Exception exception)
+			{
+				server.Disconnect();
+				Console.WriteLine(exception.Message);
+			}
+		}
+	}
+}
