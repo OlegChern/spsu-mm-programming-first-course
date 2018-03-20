b7e273e26ef098ef6058210f173f7fa4d4c290d9
Dolgopolova Maria <dolgopolovamariia@gmail.com>
2018-03-21 02:15:36 +0500

    tasks

---
+++ Spring/task4.HashTable/task4.HashTable/List.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace task4.HashTable
+{
+    public class List<T>
+    {
+        private class ListElement
+        {
+            private T value;
+
+            public T Value
+            {
+                get
+                {
+                    return value;
+                }
+                private set
+                {
+                    this.value = value;
+                }
+            }
+
+            private int key;
+
+            public int Key
+            {
+                get
+                {
+                    return key;
+                }
+                private set
+                {
+                    key = value;
+                }
+            }
+
+            private ListElement next;
+
+            public ListElement Next
+            {
+                get
+                {
+                    return next;
+                }
+                set
+                {
+                    next = value;
+                }
+            }
+
+            public ListElement(T value, int key, ListElement next)
+            {
+                this.Value = value;
+                this.Key = key;
+                this.Next = next;
+            }
+        }
+
+        private int length;
+
+        public int Length
+        {
+            get
+            {
+                return length;
+            }
+            private set
+            {
+                length = value;
+            }
+        }
+
+        private ListElement first;
+
+        public List()
+        {
+            this.first = null;
+            this.Length = 0;
+        }
+
+        public void AddFirst(T value, int key)
+        {
+            var newElement = new ListElement(value, key, first);
+            first = newElement;
+            Length++;
+        }
+
+        public T GetValueByKey(int key, ref bool wasFound)
+        {
+            ListElement temp = first;
+            while(temp != null)
+            {
+                if (temp.Key == key)
+                {
+                    wasFound = true;
+                    return temp.Value;
+                }
+                temp = temp.Next;
+            }
+            wasFound = false;
+            return default(T);
+        }
+
+        public void deleteElement(int key)
+        {
+            while ((first != null) && (first.Key == key))
+            {
+                first = first.Next;
+                Length--;
+            }
+
+            ListElement temp = first;
+            while (temp != null)
+            {
+                while ((temp.Next != null) && (temp.Next.Key == key))
+                {
+                    temp.Next = temp.Next.Next;
+                    Length--;
+                }
+                temp = temp.Next;
+            }
+        }
+
+        public bool Contains(int key)
+        {
+            bool contains = false;
+            GetValueByKey(key, ref contains);
+            return contains;
+        }
+
+        public void PrintList()
+        {
+            if(first == null)
+            {
+                return;
+            }
+            ListElement temp = first;
+            while (temp != null)
+            {
+                Console.Write(temp.Value);
+                Console.Write(" ");
+                temp = temp.Next;
+            }
+            Console.WriteLine();
+        }
+
+        public void DeleteList()
+        {
+            first = null;
+        }
+    }
+}
