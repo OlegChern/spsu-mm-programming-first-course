5a662c3ea3f8a57d08d940a17bfebf472c212a07
Dolgopolova Maria <dolgopolovamariia@gmail.com>
2018-03-21 00:33:26 +0500

    task 4

---
+++ Spring/task4.HashTable/task4.HashTable/HashTable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace task4.HashTable
+{
+    public class HashTable<T>
+    {
+        private int numberOfLists;
+
+        private List<T>[] lists;
+
+        public HashTable(int numberOfLists)
+        {
+            this.numberOfLists = numberOfLists;
+            this.lists = new List<T>[numberOfLists];
+            for (int i = 0; i < numberOfLists; i++)
+            {
+                this.lists[i] = new List<T>();
+            }
+        }
+
+        private int HashFunction(int key) => (Math.Abs(key) % numberOfLists);
+
+        public void AddElement(T value, int key)
+        {
+            lists[HashFunction(key)].AddFirst(value, key);
+        }
+
+        public void DeleteElement(int key)
+        {
+            lists[HashFunction(key)].deleteElement(key);
+        }
+
+        public T GetValueByKey(int key, ref bool wasFound) => (lists[HashFunction(key)].GetValueByKey(key, ref wasFound));
+
+        public bool Contains(int key) => (lists[HashFunction(key)].Contains(key));
+
+        public void PrintTable()
+        {
+            for (int i = 0; i < numberOfLists; i++)
+            {
+                lists[i].PrintList();
+            }
+        }
+
+        public void DeleteTable()
+        {
+            for (int i = 0; i < numberOfLists; i++)
+            {
+                lists[i].DeleteList();
+            }
+            lists = null;
+        }
+    }
+}
