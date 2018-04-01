458a0c2c547d92669aaddc14744619695cb71428
Kirill <kirill_gla@mail.ru>
2018-04-01 23:03:59 +0300

    - started work on Task7

---
+++ Spring/Task7/Task7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Task7
+{
+    static class Program
+    {
+        static void Main()
+        {
+            IList<int> list = new int[12];
+            list.Add(123);
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
---
+++ Spring/Task7/WeakHashTable/WeakHashTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace WeakHashTable
+{
+    public sealed class WeakHashTable<T>
+        where T : class
+    {
+        const int InitialBuckets = 64;
+
+        int bucketsCount;
+        
+        IList<IList<WeakReference<T>>> Buckets { get; set; }
+
+        int MillisecondsToKeep { get; }
+
+        public WeakHashTable() : this(300)
+        {
+        }
+
+        public WeakHashTable(int time)
+        {
+            bucketsCount = InitialBuckets;
+            Buckets = new IList<WeakReference<T>>[InitialBuckets];
+            MillisecondsToKeep = time;
+        }
+
+        /// <summary>
+        /// Doubles length of Buckets
+        /// </summary>
+        void Extend()
+        {
+            var oldBuckets = Buckets;
+            bucketsCount *= 2;
+            Buckets = new IList<WeakReference<T>>[bucketsCount];
+            //Buckets.Add();
+        }
+    }
+}
