b93f8e439f4f86edee36d50196b7173e395240a5
usik-vladislav <usik.vladislav.s@gmail.com>
2018-04-15 00:19:38 +0300

    Added Task 4

---
+++ Spring/Task 4/Task 2.4/DoublyLinkedList.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2._4
+{
+    class DoublyLinkedList<T> : IEnumerable<T>
+        where T : IEquatable<T>
+    {
+        public ListItem<T> Head { get; set; }
+        public ListItem<T> Tail { get; set; }
+        public int Count { get; set; }
+
+        public T this[int index]
+        {
+            get
+            {
+                if ((index < 0) || (index >= Count))
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                int i = 0;
+                foreach ( var e in this)
+                {
+                    if (i == index)
+                    {
+                        return e;
+                    }
+                    i++;
+                }
+                throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                if ((index < 0) || (index >= Count))
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                var item = Head;
+                for (int i = 0; i < index; i++)
+                {
+                    item = item.Next;
+                }
+                item.Value = value;
+            }
+        }
+
+        public void Print()
+        {
+            Console.Write("Вывод списка: ");
+            if (Count == 0)
+            {
+                Console.WriteLine("Список пуст.");
+                return;
+            }
+            foreach(var e in this)
+            {
+                Console.Write("{0} ", e);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Количество элементов: {0}", this.Count);
+        }
+
+        public void Add(T value)
+        {
+            if (Count == 0)
+            {
+                var item = new ListItem<T>(value, null, null);
+                Head = Tail = item;
+                Count++;
+            }
+            else
+            {
+                var item = new ListItem<T>(value, null, Tail);
+                Tail.Next = item;
+                Tail = item;
+                Count++;
+            }
+        }
+
+        public void AddFirst(T value)
+        {
+            if (Count == 0)
+            {
+                var item = new ListItem<T>(value, null, null);
+                Head = Tail = item;
+                Count++;
+            }
+            else
+            {
+                var item = new ListItem<T>(value, Head, null);
+                Head.Prev = item;
+                Head = item;
+                Count++;
+            }
+        }
+
+        public bool Contains(T value)
+        {
+            foreach (var e in this)
+            {
+                if (e.Equals(value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var item = Head;
+            while (item != null)
+            {
+                yield return item.Value;
+                item = item.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Remove(T value)
+        {
+            if (!this.Contains(value))
+            {
+                throw new ArgumentException();
+            }
+            if (Head == Tail)
+            {
+                Head = Tail = null;
+                Count--;
+                return;
+            }
+            var item = Head;
+            while (!(item.Value).Equals(value))
+            {
+                item = item.Next;
+            }
+            if (item.Prev == null)
+            {
+                (item.Next).Prev = null;
+                Head = item.Next;
+            }
+            else
+            {
+                (item.Next).Prev = item.Prev;
+                (item.Prev).Next = item.Next;
+            }
+            Count--;
+        }
+
+        public void Clear()
+        {
+            foreach(var e in this)
+            {
+                this.Remove(e);
+            }
+        }
+
+    }
+}
---
+++ Spring/Task 4/Task 2.4/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2._4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Создание листа интов.");
+
+            var list1 = new DoublyLinkedList<int>();
+
+            list1.Print();
+
+            Console.WriteLine("Добавление элементов с конца.");
+
+            for (int i = 0; i < 7; i++)
+            {
+                list1.Add(i);
+            }
+
+            list1.Print();
+
+            Console.WriteLine("Добавление элементов с начала.");
+
+            for (int i = 3; i < 6; i++)
+            {
+                list1.AddFirst(i);
+            }
+
+            list1.Print();
+
+            Console.WriteLine("Удаление элементов 2, 4 и 4");
+
+            list1.Remove(2);
+            list1.Remove(4);
+            list1.Remove(4);
+
+            list1.Print();
+
+            Console.WriteLine("Изменение значений по индексу.");
+
+            list1[4] = 66;
+            list1[6] = 128;
+
+            list1.Print();
+
+            Console.WriteLine("Получение значения по индексу.");
+
+            int Value = 0;
+            Console.Write("Value = {0} , Value = list1[6] , ", Value);
+            Value = list1[6];
+            Console.WriteLine("Value = {0}", Value);
+
+            Console.WriteLine("Удаление всех элементов.");
+
+            list1.Clear();
+
+            list1.Print();
+
+            Console.WriteLine("Создание списка строк");
+            var list2 = new DoublyLinkedList<string>();
+
+            list2.Print();
+
+            Console.WriteLine("Добавление элементов");
+
+            list2.Add("Середина");
+            list2.Add("Конец");
+            list2.AddFirst("Начало");
+
+            list2.Print();
+
+            Console.WriteLine("Поиск до и после удаления элемента:");
+
+            list2.Print();
+
+            Console.WriteLine("Поиск Середина: {0}", list2.Contains("Середина"));
+
+            Console.WriteLine("Удаление.");
+
+            list2.Remove("Середина");
+
+            list2.Print();
+
+            Console.WriteLine("Поиск Середина: {0}", list2.Contains("Середина"));
+
+        }
+    }
+}
