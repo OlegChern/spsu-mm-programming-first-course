40266fefac2cdb04a726007e5efb1a153dddf0b9
Dolgopolova Maria <dolgopolovamariia@gmail.com>
2018-05-07 20:30:58 +0500

    Task 8

---
+++ Spring/Task8/Task8/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Interface;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Type> collectionTypes = null;
+            bool correctLocation = false;
+            while (!correctLocation)
+            {
+                Console.WriteLine("Please, enter the location of the file");
+                string location = Console.ReadLine();
+                try
+                {
+                    collectionTypes = Assembly.LoadFile(location).ExportedTypes;
+                }
+                catch
+                {
+                    Console.WriteLine("Incorrect location");
+                    continue;
+                }
+                correctLocation = true;
+            }
+
+            var collectionSuitableTypes = from temp in collectionTypes
+                                          where temp.GetInterfaces().Contains(typeof(TempInterface))
+                                          select temp;
+
+            var collectionConstructors = from temp in collectionSuitableTypes
+                                         select temp.GetConstructor(new Type[0]);
+
+            var collectionObjects = from temp in collectionConstructors
+                                    select (TempInterface)temp.Invoke(new object[0]);
+
+            foreach (var temp in collectionObjects)
+            {
+                temp.DoSomething();
+            }
+        }
+    }
+}
---
+++ Spring/Task8/TestClasses/Classes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Interface;
+
+namespace TestClasses
+{
+    public class ClassA : TempInterface
+    {
+        public ClassA()
+        { }
+    
+        public void DoSomething()
+        {
+            Console.WriteLine("Class A");
+        }
+    }
+
+    public class ClassB : TempInterface
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("Class B");
+        }
+    }
+
+    public class ClassC
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("Class C");
+        }
+    }
+}
