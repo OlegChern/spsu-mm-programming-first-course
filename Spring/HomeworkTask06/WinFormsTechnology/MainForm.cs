abbd4bef1a03ef51d71642f3bf3ea00210c21b5a
sultim222 <sultim.tsyrendashiev@gmail.com>
2018-05-10 23:22:47 +0300

    - model-view done through events

--- Spring/HomeworkTask06/WinFormsTechnology/MainForm.cs
+++ Spring/HomeworkTask06/WinFormsTechnology/MainForm.cs
@@ -7,13 +7,9 @@ namespace WinFormsTechnology
 {
     public partial class MainForm : Form
     {
-        private Model model;
-        private Controller controller;
-
         private Graphics graphics;
         private Pen pen;
 
-        #region initializing
         public MainForm()
         {
             InitializeComponent();
@@ -22,47 +18,38 @@ namespace WinFormsTechnology
             graphics = PictureBox.CreateGraphics();
             graphics.SmoothingMode = SmoothingMode.AntiAlias;
 
-            model = new Model(this, graphics);
-            controller = new Controller(model);
+            Model model = new Model(PictureBox, graphics);
 
+            // add available curves
             CurveListComboBox.Items.AddRange(model.Curves);
 
-            this.CurveListComboBox.SelectedIndexChanged += new System.EventHandler(controller.ComboBoxSelectedIndexChanged);
-            this.BtnScaleUp.Click += new System.EventHandler(controller.BtnScaleUpClick);
-            this.BtnScaleDown.Click += new System.EventHandler(controller.BtnScaleDownClick);
+            // events
+            CurveListComboBox.SelectedIndexChanged += new EventHandler(model.ComboBoxSelectedIndexChanged);
+            BtnScaleUp.Click += new EventHandler(model.BtnScaleUpClick);
+            BtnScaleDown.Click += new EventHandler(model.BtnScaleDownClick);
 
+            // events
             model.EnableScaleDownBtn += (bool x) => BtnScaleDown.Enabled = x;
             model.EnableScaleUpBtn += (bool x) => BtnScaleUp.Enabled = x;
             model.SetPenWidth += (float x) => pen.Width = x;
-        }
-        #endregion
 
-        #region drawing
-        public void DrawCurve(PointF[] points)
-        {
-            graphics.DrawCurve(pen, points);
-        }
+            model.DrawCurve += (PointF[] points) => graphics.DrawCurve(pen, points);
+            model.DrawEllipse += (float x, float y, float width, float height) => graphics.DrawEllipse(pen, x, y, width, height);
+            model.DrawLine += (float x1, float y1, float x2, float y2) => graphics.DrawLine(pen, x1, y1, x2, y2);
 
-        public void DrawEllipse(float x, float y, float width, float height)
-        {
-            graphics.DrawEllipse(pen, x, y, width, height);
+            model.Clear += Clear;
+            model.DrawAxis += DrawAxis;
         }
 
-        public void DrawLine(float x1, float y1, float x2, float y2)
-        {
-            graphics.DrawLine(pen, x1, y1, x2, y2);
-        }
-
-        public void DrawAxis(float width, float height)
-        {
-            graphics.DrawLine(pen, 0, -height, 0, height);
-            graphics.DrawLine(pen, -width, 0, width, 0);
-        }
-
-        public void Clear()
+        private void Clear()
         {
             graphics.Clear(Color.White);
         }
-        #endregion
+
+        private void DrawAxis(float halfWidth, float halfHeight)
+        {
+            graphics.DrawLine(pen, 0, -halfHeight, 0, halfHeight);
+            graphics.DrawLine(pen, -halfWidth, 0, halfWidth, 0);
+        }
     }
 }
--- Spring/HomeworkTask06/WinFormsTechnology/Model.cs
+++ Spring/HomeworkTask06/WinFormsTechnology/Model.cs
@@ -14,9 +14,22 @@ namespace WinFormsTechnology
         public delegate void Enable(bool enabled);
         public delegate void SetFloat(float value);
 
+        public delegate void CurveDrawingHandler(PointF[] points);
+        public delegate void EllipseDrawingHandler(float x, float y, float width, float height);
+        public delegate void LineDrawingHandler(float x1, float y1, float x2, float y2);
+
+        public delegate void ClearingHandler();
+        public delegate void AxisDrawingHandler(float halfWidth, float halfHeight);
+
         public event Enable EnableScaleUpBtn;
         public event Enable EnableScaleDownBtn;
         public event SetFloat SetPenWidth;
+
+        public event CurveDrawingHandler DrawCurve;
+        public event EllipseDrawingHandler DrawEllipse;
+        public event LineDrawingHandler DrawLine;
+        public event ClearingHandler Clear;
+        public event AxisDrawingHandler DrawAxis;
 
         #region constants
         private const float ScaleMultiplier = 1.25f;
@@ -30,14 +43,17 @@ namespace WinFormsTechnology
         private List<CurveBase> curves;
         private CurveBase selected;
 
+        private PictureBox pictureBox;
         private Graphics graphics;
+
         private Matrix originalTransform;
         private float scale;
-
-        private MainForm view;
         #endregion
 
         #region properties
+        /// <summary>
+        /// Array of all curves
+        /// </summary>
         public CurveBase[] Curves
         {
             get
@@ -46,19 +62,25 @@ namespace WinFormsTechnology
             }
         }
 
+        /// <summary>
+        /// PictureBox half width
+        /// </summary>
         private float HalfBoxWidth
         {
             get
             {
-                return (view.PictureBox.Width) / 2;
+                return (pictureBox.Width) / 2;
             }
         }
 
+        /// <summary>
+        /// PictureBox half height
+        /// </summary>
         private float HalfBoxHeight
         {
             get
             {
-                return (view.PictureBox.Height) / 2;
+                return (pictureBox.Height) / 2;
             }
         }
 
@@ -85,8 +107,11 @@ namespace WinFormsTechnology
         }
         #endregion
 
-        public Model(MainForm view, Graphics graphics)
+        public Model(PictureBox pictureBox, Graphics graphics)
         {
+            this.pictureBox = pictureBox;
+            this.graphics = graphics;
+
             curves = new List<CurveBase>();
 
             curves.Add(new Line());
@@ -95,24 +120,24 @@ namespace WinFormsTechnology
             curves.Add(new Parabola());
             curves.Add(new Hyperbola());
 
-            this.view = view;
-            this.graphics = graphics;
-            this.graphics.TranslateTransform(HalfBoxWidth, HalfBoxHeight);
-
-            originalTransform = this.graphics.Transform.Clone();
+            graphics.TranslateTransform(HalfBoxWidth, HalfBoxHeight);
+            originalTransform = graphics.Transform.Clone();
 
             scale = 1f;
             MaxScale = (float)Math.Pow(ScaleMultiplier, MaxScalePower);
             MinScale = (float)Math.Pow(ScaleMultiplier, -MaxScalePower);
         }
 
-        #region drawing
-        public void Draw(object selected)
+        /// <summary>
+        /// ComboBox event
+        /// </summary>
+        public void ComboBoxSelectedIndexChanged(object sender, EventArgs e)
         {
-            this.selected = (CurveBase)selected;
+            selected = (CurveBase)((ComboBox)sender).SelectedItem;
             Draw();
         }
 
+        #region drawing
         /// <summary>
         /// Draws selected curve
         /// </summary>
@@ -123,8 +148,8 @@ namespace WinFormsTechnology
                 return;
             }
 
-            view.Clear();
-            view.DrawAxis(ScaledHalfBoxWidth, ScaledHalfBoxHeight);
+            Clear();
+            DrawAxis(ScaledHalfBoxWidth, ScaledHalfBoxHeight);
 
             switch (selected.Type)
             {
@@ -167,7 +192,7 @@ namespace WinFormsTechnology
                     continue;
                 }
 
-                view.DrawCurve(points[i]);
+                DrawCurve(points[i]);
             }
         }
 
@@ -177,7 +202,7 @@ namespace WinFormsTechnology
             float offsetX = -ellipse.SemiMajorAxis / 2;
             float offsetY = -ellipse.SemiMinorAxis / 2;
 
-            view.DrawEllipse(offsetX, offsetY, ellipse.SemiMajorAxis, ellipse.SemiMinorAxis);
+            DrawEllipse(offsetX, offsetY, ellipse.SemiMajorAxis, ellipse.SemiMinorAxis);
         }
 
         private void Draw(Line line)
@@ -186,12 +211,15 @@ namespace WinFormsTechnology
             float length = 2 * (new Vector2(ScaledHalfBoxWidth, ScaledHalfBoxHeight)).Length;
             Vector2 lineDir = line.Direction * length;
 
-            view.DrawLine(-lineDir.X, lineDir.Y, lineDir.X, -lineDir.Y);
+            DrawLine(-lineDir.X, lineDir.Y, lineDir.X, -lineDir.Y);
         }
         #endregion
 
         #region scaling
-        public void ScaleUp()
+        /// <summary>
+        /// Scaling up event
+        /// </summary>
+        public void BtnScaleUpClick(object sender, EventArgs e)
         {
             if (scale <= MinScale)
             {
@@ -206,7 +234,10 @@ namespace WinFormsTechnology
             }
         }
 
-        public void ScaleDown()
+        /// <summary>
+        /// Scaling down event
+        /// </summary>
+        public void BtnScaleDownClick(object sender, EventArgs e)
         {
             if (scale >= MaxScale)
             {
