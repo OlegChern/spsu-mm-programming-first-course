acc360883ab29e095abea8257d3180b6938b0c77
usik-vladislav <usik.vladislav.s@gmail.com>
2018-05-13 18:51:04 +0300

    Changed Task 7

--- Spring/Task 7/Task 7/Properties/AssemblyInfo.cs
+++ Spring/Task 7/Task 7/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("ConsoleApp4")]
+[assembly: AssemblyTitle("Task 7")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ConsoleApp4")]
+[assembly: AssemblyProduct("Task 7")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
