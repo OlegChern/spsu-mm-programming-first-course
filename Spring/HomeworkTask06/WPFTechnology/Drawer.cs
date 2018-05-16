ea17efde3de70e134bc9b5658ddd8d25d4a74068
sultim222 <sultim.tsyrendashiev@gmail.com>
2018-05-16 22:42:25 +0300

    - added new files

---
+++ Spring/HomeworkTask06/WPFTechnology/Drawer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTechnology
+{
+    public class Drawer : Image
+    {
+        protected override void OnRender(DrawingContext dc)
+        {
+            dc.DrawLine(new Pen(), new Point(-10, -10), new Point(10, 10));
+
+            base.OnRender(dc);
+        }
+    }
+}
---
+++ Spring/HomeworkTask06/WPFTechnology/Model.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Collections.ObjectModel;
+
+using AlgebraicCurveLibrary;
+using AlgebraicCurveLibrary.Exapmles;
+
+namespace WPFTechnology
+{
+    public class Model
+    {
+        public delegate void Enable(bool enabled);
+
+        public event Enable EnableScaleUpBtn;
+        public event Enable EnableScaleDownBtn;
+
+        const float ScaleMultiplier = 1.25f;
+        const int MaxScalePower = 10;
+        const int MinScalePower = -10;
+
+        private List<CurveBase> curves;
+        private CurveBase selected;
+
+        private int ScalePower;
+
+        public CurveBase[] CurvesArray
+        {
+            get
+            {
+                return curves.ToArray();
+            }
+        }
+        
+        public CurveBase SelectedCurve
+        {
+            get
+            {
+                return selected;
+            }
+            set
+            {
+                selected = value;
+            }
+        }
+
+        public Model()
+        {
+            curves = new List<CurveBase>();
+
+            curves.Add(new Line());
+            curves.Add(new Circle());
+            curves.Add(new Ellipse());
+            curves.Add(new Parabola());
+            curves.Add(new Hyperbola());
+        }
+
+        #region scaling
+        /// <summary>
+        /// Scaling up event
+        /// </summary>
+        public void ScaleUp(object sender, EventArgs e)
+        {
+            if (ScalePower <= MinScalePower)
+            {
+                EnableScaleDownBtn(true);
+            }
+
+            ScalePower++;
+            ScaleCurves(ScaleMultiplier);
+
+            if (ScalePower >= MaxScalePower)
+            {
+                EnableScaleUpBtn(false);
+            }
+        }
+
+        /// <summary>
+        /// Scaling down event
+        /// </summary>
+        public void ScaleDown(object sender, EventArgs e)
+        {
+            if (ScalePower >= MaxScalePower)
+            {
+                EnableScaleUpBtn(true);
+            }
+
+            ScalePower--;
+            ScaleCurves(1 / ScaleMultiplier);
+
+            if (ScalePower <= MinScalePower)
+            {
+                EnableScaleDownBtn(false);
+            }
+        }
+
+        private void ScaleCurves(float scale)
+        {
+            /*this.scale *= scale;
+
+            Matrix newTransform = originalTransform.Clone();
+            newTransform.Scale(this.scale, this.scale);
+
+            graphics.Transform = newTransform;
+            SetPenWidth(1 / this.scale);
+
+            Draw();*/
+        }
+        #endregion
+    }
+}
