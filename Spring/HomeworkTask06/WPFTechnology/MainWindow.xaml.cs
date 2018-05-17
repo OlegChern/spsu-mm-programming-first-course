15ac6dbae84da1816bf11e3fbc6195d60d6d4a85
sultim222 <sultim.tsyrendashiev@gmail.com>
2018-05-17 17:32:16 +0300

    - WPF technology added

--- Spring/HomeworkTask06/WPFTechnology/MainWindow.xaml.cs
+++ Spring/HomeworkTask06/WPFTechnology/MainWindow.xaml.cs
@@ -14,22 +14,13 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-using AlgebraicCurveLibrary;
-using AlgebraicCurveLibrary.Exapmles;
-
 namespace WPFTechnology
 {
-    using WPoint = System.Windows.Point;
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        delegate void Do();
-
-        Model model;
-
         public MainWindow()
         {
             InitializeComponent();
@@ -37,41 +28,26 @@ namespace WPFTechnology
             // when all loaded, start initializing model
             Loaded += delegate
             {
-                model = new Model();
+                Model model = new Model();
+                model.Canvas = DrawingCanvas;
+
+                model.EnableScaleUpBtn += (bool x) => ScaleUpBtn.IsEnabled = x;
+                model.EnableScaleDownBtn += (bool x) => ScaleDownBtn.IsEnabled = x;
+                model.DrawGeometry += BindData;
 
                 ScaleUpBtn.Click += new RoutedEventHandler(model.ScaleUp);
                 ScaleDownBtn.Click += new RoutedEventHandler(model.ScaleDown);
 
-                model.EnableScaleUpBtn += (bool x) => ScaleUpBtn.IsEnabled = x;
-                model.EnableScaleDownBtn += (bool x) => ScaleDownBtn.IsEnabled = x;
-
-                Curve parabola = new Parabola();
-
-                PointF[][] points = parabola.GetDrawingPoints(new Vector2(0,0), new Vector2((float)DrawingCanvas.ActualWidth, (float)DrawingCanvas.ActualHeight));
-                PathFigure[] figures = new PathFigure[points.Length];
-                WPoint lastPoint = new WPoint();
-
-                for (int i = 0; i < points.Length; i++)
+                CurvesComboBox.SelectionChanged += delegate
                 {
-                    figures[i] = new PathFigure();
-
-                    List<WPoint> partPoints = new List<WPoint>();
-
-                    foreach(PointF point in points[i])
-                    {
-                        WPoint converted = new WPoint(point.X + DrawingCanvas.ActualWidth / 2, point.Y + DrawingCanvas.ActualHeight / 2);
-                        partPoints.Add(converted);
-
-                        lastPoint = converted;
-                    }
-
-                    figures[i].Segments.Add(new PolyBezierSegment(partPoints, true));
-                }
-
-                figures[points.Length - 1].StartPoint = lastPoint;
-
-                DrawingPath.Data = new PathGeometry(figures);
+                    model.SelectCurve(CurvesComboBox.SelectedItem);
+                };
             };
+        }
+
+        void BindData(Geometry geometry)
+        {
+            DrawingPath.Data = geometry;
         }
     }
 }
--- Spring/HomeworkTask06/WPFTechnology/Model.cs
+++ Spring/HomeworkTask06/WPFTechnology/Model.cs
@@ -8,25 +8,39 @@ using System.Collections.ObjectModel;
 
 using AlgebraicCurveLibrary;
 using AlgebraicCurveLibrary.Exapmles;
+using System.Windows.Media;
 
 namespace WPFTechnology
 {
+    using System.Windows.Controls;
+    using WPoint = System.Windows.Point;
+
     public class Model
     {
         public delegate void Enable(bool enabled);
+        public delegate void DrawHandler(Geometry geometry);
 
         public event Enable EnableScaleUpBtn;
         public event Enable EnableScaleDownBtn;
+        public event DrawHandler DrawGeometry;
 
+        #region constants
         const float ScaleMultiplier = 1.25f;
         const int MaxScalePower = 10;
         const int MinScalePower = -10;
+        #endregion
+
+        #region fields
+        private Canvas canvas;
 
         private List<CurveBase> curves;
         private CurveBase selected;
 
-        private int ScalePower;
+        private int scalePower;
+        private float scale;
+        #endregion
 
+        #region properties
         public CurveBase[] CurvesArray
         {
             get
@@ -34,28 +48,123 @@ namespace WPFTechnology
                 return curves.ToArray();
             }
         }
-        
-        public CurveBase SelectedCurve
+
+        public Canvas Canvas
         {
             get
             {
-                return selected;
+                return canvas;
             }
             set
             {
-                selected = value;
+                canvas = value;
             }
         }
+
+        private float CanvasHalfWidth
+        {
+            get
+            {
+                return (float)canvas.ActualWidth / 2;
+            }
+        }
+
+        private float CanvasHalfHeight
+        {
+            get
+            {
+                return (float)canvas.ActualHeight / 2;
+            }
+        }
+        #endregion
 
         public Model()
         {
             curves = new List<CurveBase>();
 
             curves.Add(new Line());
-            curves.Add(new Circle());
-            curves.Add(new Ellipse());
+            curves.Add(new Circle(50));
+            curves.Add(new Ellipse(130, 50));
             curves.Add(new Parabola());
             curves.Add(new Hyperbola());
+
+            scale = 1;
+        }
+
+        public void SelectCurve(object curve)
+        {
+            selected = (CurveBase)curve;
+            Draw();
+        }
+
+        public void Draw()
+        {
+            if (selected ==null)
+            {
+                return;
+            }
+
+            switch (selected.Type)
+            {
+                case DrawableCurveType.Line:
+                    {
+                        Line line = (Line)selected;
+
+                        float length = 2 * (new Vector2(CanvasHalfWidth, CanvasHalfHeight)).Length;
+                        Vector2 lineDir = line.Direction * length;
+
+                        DrawGeometry(new LineGeometry(
+                            new WPoint(-lineDir.X + CanvasHalfWidth, lineDir.Y + CanvasHalfWidth),
+                            new WPoint(lineDir.X + CanvasHalfWidth, -lineDir.Y + CanvasHalfWidth)));
+
+                        break;
+                    }
+                case DrawableCurveType.Ellipse:
+                    {
+                        Ellipse ellipse = (Ellipse)selected;
+                        WPoint center = new WPoint(CanvasHalfWidth, CanvasHalfHeight);
+
+                        DrawGeometry(new EllipseGeometry(center, ellipse.SemiMajorAxis * scale, ellipse.SemiMinorAxis * scale));
+                        break;
+                    }
+                case DrawableCurveType.Curve:
+                    {
+                        PointF[][] points = ((Curve)selected).GetDrawingPoints(
+                            new Vector2(-CanvasHalfWidth / scale, CanvasHalfHeight / scale),
+                            new Vector2(CanvasHalfWidth / scale, -CanvasHalfHeight / scale));
+
+                        PathFigure[] figures = new PathFigure[points.Length];
+
+                        for (int i = 0; i < points.Length; i++)
+                        {
+                            figures[i] = new PathFigure();
+
+                            List<WPoint> partPoints = new List<WPoint>();
+
+                            if (points[i].Length > 0)
+                            {
+                                figures[i].StartPoint = Convert(points[i][0]);
+                            }
+
+                            foreach (PointF point in points[i])
+                            {
+                                WPoint converted = Convert(point);
+                                partPoints.Add(converted);
+                            }
+
+                            figures[i].Segments.Add(new PolyBezierSegment(partPoints, true));
+                        }
+
+                        DrawGeometry(new PathGeometry(figures));
+
+                        break;
+                    }
+            }
+        }
+
+        private WPoint Convert(PointF point)
+        {
+            return new WPoint(point.X * scale + CanvasHalfWidth, point.Y * scale + CanvasHalfHeight);
         }
 
         #region scaling
@@ -64,18 +173,20 @@ namespace WPFTechnology
         /// </summary>
         public void ScaleUp(object sender, EventArgs e)
         {
-            if (ScalePower <= MinScalePower)
+            if (scalePower <= MinScalePower)
             {
                 EnableScaleDownBtn(true);
             }
 
-            ScalePower++;
-            ScaleCurves(ScaleMultiplier);
+            scalePower++;
+            scale *= ScaleMultiplier;
 
-            if (ScalePower >= MaxScalePower)
+            if (scalePower >= MaxScalePower)
             {
                 EnableScaleUpBtn(false);
             }
+
+            Draw();
         }
 
         /// <summary>
@@ -83,31 +194,20 @@ namespace WPFTechnology
         /// </summary>
         public void ScaleDown(object sender, EventArgs e)
         {
-            if (ScalePower >= MaxScalePower)
+            if (scalePower >= MaxScalePower)
             {
                 EnableScaleUpBtn(true);
             }
 
-            ScalePower--;
-            ScaleCurves(1 / ScaleMultiplier);
+            scalePower--;
+            scale /= ScaleMultiplier;
 
-            if (ScalePower <= MinScalePower)
+            if (scalePower <= MinScalePower)
             {
                 EnableScaleDownBtn(false);
             }
-        }
 
-        private void ScaleCurves(float scale)
-        {
-            /*this.scale *= scale;
-
-            Matrix newTransform = originalTransform.Clone();
-            newTransform.Scale(this.scale, this.scale);
-
-            graphics.Transform = newTransform;
-            SetPenWidth(1 / this.scale);
-
-            Draw();*/
+            Draw();
         }
         #endregion
     }
