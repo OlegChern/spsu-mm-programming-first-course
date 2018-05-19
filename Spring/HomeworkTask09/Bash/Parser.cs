d9157bbc03eb6d1aa9ed2a4780fceae9515dada2
sultim222 <sultim.tsyrendashiev@gmail.com>
2018-05-19 19:07:40 +0300

    - added task 9

---
+++ Spring/HomeworkTask09/Bash/Parser.cs
@@ -0,0 +1,93 @@
+﻿using Bash.Commands;
+
+namespace Bash
+{
+    class Parser
+    {
+        /*private static Parser instance;
+
+        public static Parser Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Parser();
+                }
+
+                return instance;
+            }
+        }
+
+        private Parser() { }*/
+
+        public static bool Parse(string str, out ICommand command)
+        {
+            command = null;
+
+            if (str.Length == 0)
+            {
+                return false;
+            }
+
+            string[] splitted = str.Split(' ');
+
+            if (splitted.Length == 0)
+            {
+                return false;
+            }
+
+            // todo
+            if (str[0] == '&')
+            {
+
+            }
+
+            string arg = Concatenate(1, splitted);
+
+            switch (splitted[0])
+            {
+                case "echo":
+                    {
+                        command = new Echo(arg);
+                        return true;
+                    }
+                case "exit":
+                    {
+                        command = new Exit();
+                        return true;
+                    }
+                case "pwd":
+                    {
+                        command = new Pwd();
+                        return true;
+                    }
+                case "cat":
+                    {
+                        command = new Cat(arg);
+                        return true;
+                    }
+                case "wc":
+                    {
+                        command = new Wc(arg);
+                        return true;
+                    }
+            }
+
+            command = new SystemProcess(Concatenate(0, splitted));
+            return true;
+        }
+
+        private static string Concatenate(int startIndex, string[] strs)
+        {
+            string result = string.Empty;
+
+            for (int i = startIndex; i < strs.Length; i++)
+            {
+                result += strs[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Spring/HomeworkTask09/Bash/Variables.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bash
+{
+    struct Variable
+    {
+        string name;
+        string value;
+
+        public string Name
+        {
+           get
+            {
+                return name;
+            }
+        }
+
+        public string Value
+        {
+            get
+            {
+                return value;
+            }
+        }
+
+        public Variable(string name, string value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+    }
+}
