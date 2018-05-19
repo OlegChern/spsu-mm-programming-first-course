7b2f9f955536a14f7790ecb7ccaac9a40d95417f
sultim222 <sultim.tsyrendashiev@gmail.com>
2018-05-19 19:05:14 +0300

    - added commmands

--- Spring/HomeworkTask09/Bash/Bash.cs
+++ Spring/HomeworkTask09/Bash/Bash.cs
@@ -35,6 +35,11 @@ namespace Bash
 
         public void Start()
         {
+            Console.WriteLine("Bash started.");
+            ShowHelp();
+
+            List<Variable> variables = new List<Variable>();
+
             isExecuting = true;
 
             while (isExecuting)
@@ -49,22 +54,10 @@ namespace Bash
                         context.SetCommand(command);
                         context.ExecuteCommand();
                     }
-                    else
-                    {
-                        ProcessStartInfo info = new ProcessStartInfo();
-                        info.FileName ="/k"+str ;
-                        info.Arguments = str;
-                        info.CreateNoWindow = true;
-                        info.RedirectStandardOutput = true;
-                        info.UseShellExecute = false;
-
-
-                        Process.Start(info);
-                    }
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine("> " + ex.ToString());
+                    Console.WriteLine("> " + ex.Message);
                 }
             }
         }
@@ -72,6 +65,20 @@ namespace Bash
         public void Stop()
         {
             isExecuting = false;
+        }
+
+        private void ShowHelp()
+        {
+            Console.WriteLine("Commands:");
+            Console.WriteLine("  echo - show argument(-s)");
+            Console.WriteLine("  exit - stop Bash");
+            Console.WriteLine("  pwd - show current directory");
+            Console.WriteLine("  cat[FILENAME] - show file content");
+            Console.WriteLine("  wc[FILENAME] - show number of strings, words and bytes");
+
+            Console.WriteLine("Operators:");
+            Console.WriteLine("  $ - assigning and using of local variables");
+            Console.WriteLine("  | - commmands pipelining");
         }
     }
 }
