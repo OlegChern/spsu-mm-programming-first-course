7b3811506f91f1ad29340a07b6712c263e3f94a4
sultim222 <sultim.tsyrendashiev@gmail.com>
2018-05-21 15:00:33 +0300

    - changed App.config

--- Spring/HomeworkTask10/HomeworkTask10/Program.cs
+++ Spring/HomeworkTask10/HomeworkTask10/Program.cs
@@ -25,9 +25,9 @@ namespace HomeworkTask10
 
             for (int i = 0; i < 40; i++)
             {
-                bots[0] = container.Resolve<FearfulBot>();
-                bots[1] = container.Resolve<RiskyBot>();
-                bots[2] = container.Resolve<SmartBot>();
+                bots[0] = container.Resolve<Player>("fearfulBot");
+                bots[1] = container.Resolve<Player>("riskyBot");
+                bots[2] = container.Resolve<Player>("smartBot");
 
                 Game.Instance.Start(bots);
 
