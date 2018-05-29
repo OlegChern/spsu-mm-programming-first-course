eb80a5a9a409016884712d72efb24537a43ead86
olesyagogina <olesyagogina@mail.ru>
2018-05-29 15:44:10 +0300

    modified task 8

---
+++ Spring/task8/src/TestClasses/ThirdClass.java
@@ -0,0 +1,11 @@
+package TestClasses;
+
+import TestInterface.Interface;
+
+public class ThirdClass implements Interface {
+
+    public void test() {
+        System.out.println("Tested third class");
+    }
+
+}
