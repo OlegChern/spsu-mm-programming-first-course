a3976edb4992fb90e124e8228239547b5ffddf2b
Aleksandr <bash.spbu@gmail.com>
2018-06-01 21:55:06 +0300

    Commentator is now deactivatable in Task 3 (BlackJack)

--- Spring/Task 3 (Blackjack)/src/Commentator.java
+++ Spring/Task 3 (Blackjack)/src/Commentator.java
@@ -2,20 +2,30 @@ import java.io.PrintWriter;
 
 public class Commentator {
     private PrintWriter pw;
+    private boolean isMute;
     
     public Commentator(PrintWriter pw) {
         this.pw = pw;
+        this.isMute = false;
     }
     
     public void comment(String player, String action) {
-        if (pw != null) {
+        if (pw != null && !isMute) {
             pw.println(player + " " + action);
         }
     }
     
     public void comment(String message) {
-        if (pw != null) {
+        if (pw != null && !isMute) {
             pw.println(message);
         }
+    }
+    
+    public boolean isMute() {
+        return isMute;
+    }
+    
+    public void mute(boolean mute) {
+        isMute = mute;
     }
 }
