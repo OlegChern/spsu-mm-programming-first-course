83cfdc9e339cfde5aadb6e024acb58783a0f5ca2
olchern <ol1998_@mail.ru>
2018-06-08 13:17:21 +0300

    Added ENUM fields to task 3

--- Spring/Task 3/src/BlackjackGame/Card.java
+++ Spring/Task 3/src/BlackjackGame/Card.java
@@ -10,62 +10,7 @@ public class Card {
 
         this.rank = rank;
         this.suit = suit;
-
-        switch (this.rank) {
-
-            case Two: {
-                this.value = 2;
-                break;
-            }
-            case Three: {
-                this.value = 3;
-                break;
-            }
-            case Four: {
-                this.value = 4;
-                break;
-            }
-            case Five: {
-                this.value = 5;
-                break;
-            }
-            case Six: {
-                this.value = 6;
-                break;
-            }
-            case Seven: {
-                this.value = 7;
-                break;
-            }
-            case Eight: {
-                this.value = 8;
-                break;
-            }
-            case Nine: {
-                this.value = 9;
-                break;
-            }
-            case Ten: {
-                this.value = 10;
-                break;
-            }
-            case Jack: {
-                this.value = 10;
-                break;
-            }
-            case Queen: {
-                this.value = 10;
-                break;
-            }
-            case King: {
-                this.value = 10;
-                break;
-            }
-            case Ace: {
-                this.value = 1;
-                break;
-            }
-        }
+        this.value = rank.getValue();
 
     }
 
--- Spring/Task 3/src/BlackjackGame/Ranks.java
+++ Spring/Task 3/src/BlackjackGame/Ranks.java
@@ -1,17 +1,26 @@
 package BlackjackGame;
 
 public enum Ranks {
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-    Ten,
-    Jack,
-    Queen,
-    King,
-    Ace
+    Two(2),
+    Three(3),
+    Four(4),
+    Five(5),
+    Six(6),
+    Seven(7),
+    Eight(8),
+    Nine(9),
+    Ten(10),
+    Jack(10),
+    Queen(10),
+    King(10),
+    Ace(1);
+
+    Ranks(int value) {
+        this.value = value;
+    }
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
 };
