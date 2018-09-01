9569c37cc4d9d8bc2983a9dcc17aa5351a42a82b
Vladimir Korolikhin <volodyk.99@mail.ru>
2018-09-01 23:27:18 +0300

    Update task

---
+++ Spring/task6/DrawChart/Libs/Charts/CirlceChart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Libs
+{
+    public class CircleChart : Chart
+    {
+        public CircleChart(string _name)
+        {
+            title = _name;
+        }
+        public CircleChart(string _name, bool _mirror)
+        {
+            title = _name;
+            mirror = _mirror;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public override float f(float value)
+        {
+            return (float)Math.Sqrt(-1 * (float)value * (float)value + 625);
+        }
+    }
+}
---
+++ Spring/task6/DrawChart/Libs/Charts/Points.cs
@@ -0,0 +1,18 @@
+﻿namespace Libs.Charts
+{
+    public class Point
+    {
+        public double X;
+        public double Y;
+        public Point()
+        {
+            X = 0;
+            Y = 0;
+        }
+        public Point(double _X, double _Y)
+        {
+            X = _X;
+            Y = _Y;
+        }
+    }
+}
